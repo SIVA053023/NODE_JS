a61ee11b205c949ef7db2f10e7e554b51d282826
siva krishna <sivakrishna053936@gmail.com>
2024-07-12 15:26:05 +0530

    test

---
+++ Thirdparty modules-2/Bycrypt-js/Bcryptpractice/One.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt';
+let emp={
+    ename:"Sivakrishna",
+    eid:"405",
+    esal:"56896",
+    elogid:"2358",
+    epassword:"Siva45@",
+}
+
+let salt=bcrypt.genSaltSync(10);
+let new_logid=bcrypt.hashSync(emp.elogid,salt)
+console.log(emp.elogid)
+console.log(new_logid);
+// user={...user,cc:new_cc,password:new_password}
+//emp=(...emp,elogid:new_logid)
+// console.log(emp);
+emp={...emp,elogid:new_logid}
+console.log(emp)
+let flag=bcrypt.compareSync('2358',emp.elogid)
+if(flag){
+    console.log("login success")
+}
+else{
+    console.log("login failed")
+}

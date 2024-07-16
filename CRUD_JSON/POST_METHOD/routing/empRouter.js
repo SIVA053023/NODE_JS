fc9db5b93234c2b406d6b83f1a6f20b0a655dd3d
siva krishna <sivakrishna053936@gmail.com>
2024-07-16 16:51:12 +0530

    test

---
+++ CRUD_JSON/POST_METHOD/routing/empRouter.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import fs from 'fs'
+
+let router=express.Router();
+router.post('/create',(req,resp)=>{
+
+})
+
+
+let get_employees=()=>{
+    let employees=fs.readFileSync
+}
